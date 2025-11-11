350b09d7c1acccf966b22962926c1571897acbd7
dannmaruu4-star <dannmaruu4@gmail.com>
2025-11-11 18:05:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get('id');
 
 if (userId) {
-  fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?id=' + userId)
+  fetch('https://script.google.com/macros/s/AKfycbx_upT3JIXFr_Xqth5DUT1AhL7zVOb3FXYQR6SmO4ekTr-L_Y_fsb0sVUedZmxKufBX/exec' + userId)
     .then(response => console.log('Tracked:', userId))
     .catch(error => console.error('Error:', error));
 }
